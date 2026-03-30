4cf3c771d52fd9a90b921b4e9eb9935969715f73
Manas Pandey <manaspandey@Manass-MacBook-Air.local>
2026-03-30 18:23:27 +0530

    First Commit and User Authentication

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,83 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+
+/**
+* - user register controller
+* - POST /api/auth/register
+*/
+const userRegisterController = async (req, res) => {
+    const { email, password, name } = req.body
+
+    const userAlreadyExists = await userModel.findOne({
+        email: email
+    })
+
+    if (userAlreadyExists) {
+        return res.status(422).json({
+            message: "Email already in use.",
+            status: "failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
+/**
+ * - User Login Controller
+ * - POST /api/auth/login
+  */
+
+const userLoginController = async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email }).select("+password")
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password)
+
+    if (!isValidPassword) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+
+}
+
+
+module.exports = {
+    userRegisterController,
+    userLoginController
+}
