03bf20766e2cf484bef4248f1d243ee680e32d61
Manas Pandey <manaspandey@Manass-MacBook-Air.local>
2026-04-01 22:54:50 +0530

    Implement account model, controller, routes, and auth middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 
 const authRoutes = require("./routes/auth.routes")
+const accountRoutes = require("./routes/accounts.routes")
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/accounts", accountRoutes)
 
 
 module.exports = app
